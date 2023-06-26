5073be66a1c6d6d787842c6c529ba5ad10769b7c
Antipoop <senkovskiy.nikita@hexly.ru>
2023-06-26 15:28:50 +0300

    prototype 98 add audio js file

---
+++ src/audio.js
@@ -0,0 +1,28 @@
+const audioOffice = document.getElementById('office');
+const audioCorridor = document.getElementById('corridor');
+const audioParking = document.getElementById('parking');
+const volume = 0.1
+const playOffice = () => {
+  audioOffice.volume = volume
+  audioOffice.play()
+}
+const playCorridor = () => {
+  audioCorridor.volume = volume
+  audioCorridor.play()
+}
+const playParking = () => {
+  audioParking.volume = volume
+  audioParking.play()
+}
+const stopOffice = () => {
+  audioOffice.pause()
+  audioOffice.currentTime = 0;
+}
+const stopCorridor = () => {
+  audioCorridor.pause()
+  audioCorridor.currentTime = 0;
+}
+const stopParking = () => {
+  audioParking.pause()
+  audioParking.currentTime = 0;
+}
--- src/slider.js
+++ src/slider.js
@@ -1,34 +1,6 @@
 let i = 0;
 const background= document.getElementById("bg");
 const backgrounds = ['img/background/office.jpg', 'img/background/corridor.png', 'img/background/parking.jpg']
-const audioOffice = document.getElementById('office');
-const audioCorridor = document.getElementById('corridor');
-const audioParking = document.getElementById('parking');
-const volume = 0.1
-const playOffice = () => {
-  audioOffice.volume = volume
-  audioOffice.play()
-}
-const playCorridor = () => {
-  audioCorridor.volume = volume
-  audioCorridor.play()
-}
-const playParking = () => {
-  audioParking.volume = volume
-  audioParking.play()
-}
-const stopOffice = () => {
-  audioOffice.pause()
-  audioOffice.currentTime = 0;
-}
-const stopCorridor = () => {
-  audioCorridor.pause()
-  audioCorridor.currentTime = 0;
-}
-const stopParking = () => {
-  audioParking.pause()
-  audioParking.currentTime = 0;
-}
 const next = () => {
   if (i < 7) {
     i++;
